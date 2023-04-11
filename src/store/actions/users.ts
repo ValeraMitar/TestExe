37cda3562dca37f08de5ddff7221efc7eb0b004c
ValeraMitar <v.mitar@redtrack.io>
2023-04-11 20:00:23 +0300

    Remove unused files

--- src/store/actions/users.ts
+++
@@ -1,7 +0,0 @@
-// import API from '../../api/api';
-// import {User} from "../reducers/users";
-//
-// export const loadUsersByIdAction = (id:string) => async ():Promise<User> => {
-//     return API().USERS.getUsersById(id);
-// };
-//
